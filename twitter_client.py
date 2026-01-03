0ded1d1c6b403f77d90d8ab34af08e6b198c4135
shubhi2606 <shubanish.126@gmail.com>
2026-01-04 03:19:06 +0530

    Update Twitter API keys to new environment variables

--- twitter_client.py
+++ twitter_client.py
@@ -3,10 +3,10 @@ import os
 
 def post_tweet(text: str):
     client = tweepy.Client(
-        consumer_key=os.getenv("SPANISHHOY_X_API_KEY"),
-        consumer_secret=os.getenv("SPANISHHOY_X_API_SECRET"),
-        access_token=os.getenv("SPANISHHOY_X_ACCESS_TOKEN"),
-        access_token_secret=os.getenv("SPANISHHOY_X_ACCESS_SECRET")
+        consumer_key=os.getenv("X_API_KEY"),
+        consumer_secret=os.getenv("X_API_SECRET"),
+        access_token=os.getenv("X_ACCESS_TOKEN"),
+        access_token_secret=os.getenv("X_ACCESS_SECRET")
     )
 
     response = client.create_tweet(text=text)
